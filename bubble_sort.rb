8837fafe33b592fb77839d62f470a7e1521fb013
SHEY LOUIS CHIA <shloch2007@yahoo.fr>
2019-04-02 05:03:22 +0100

    added YIELD

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -37,5 +37,50 @@ def bubbleSort(arr)
     print arr.to_s + "\n"
 end
 
-bubbleSort([4,6,1,8,2,90,7]) #testing sample
-bubbleSort([4,3,78,2,0,2])
+
+
+
+#bubbleSort([4,6,1,8,2,90,7]) #testing sample
+#bubbleSort([4,3,78,2,0,2])
+
+
+=begin
+
+same algorithm using Yield
+
+Good BLOCKS tutorial => https://www.youtube.com/watch?v=K8uhD8mtorE
+
+ > bubble_sort_by(["hi","hello","hey"]) do |left,right|
+  >   left.length - right.length
+  > end
+  => ["hi", "hey", "hello"]
+=end
+
+
+def bubble_sort_by(arr)
+
+   #puts 'yo'
+    n = arr.length
+    while (n > 1) do
+        for i in 0...n-1
+           #block
+            var = yield arr[i], arr[i+1]
+            if var > 0
+               arr[i+1], arr[i] = arr[i], arr[i+1]
+            end
+        end
+        n = n-1
+    end
+    puts arr
+    arr
+
+
+end
+
+result = bubble_sort_by(['shey','louis','chis'])  do |left,right|
+    #puts "#{left.length},#{right.length}"
+    left.length - right.length
+
+end
+
+#bubble_sort_by(['shey','louis','chis'])
